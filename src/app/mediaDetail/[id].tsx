91a4357df5e5634ffa15faf74f758e773068718d
Nabilleb <nabilgreen500@gmail.com>
2025-10-22 15:55:24 +0300

    adding media play

--- src/app/mediaDetail/[id].tsx
+++ src/app/mediaDetail/[id].tsx
@@ -1,34 +1,51 @@
-import { useLocalSearchParams } from "expo-router"
-import { View, Text } from "react-native"
-import { SafeAreaView } from "react-native-safe-area-context"
-import mediaDetailedList from '@assets/data/mediaDetailedList.json'
-import MediaInfo from "@/components/mediaDetails/MediaInfo"
+import { useLocalSearchParams } from "expo-router";
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import mediaDetailedList from '@assets/data/mediaDetailedList.json';
+import MediaInfo from "@/components/mediaDetails/MediaInfo";
+import { useVideoPlayer } from "expo-video";
+import MediaHeader from "@/components/mediaDetails/MediaHeader";
 
-function mediaDetail(){
-    const {id} = useLocalSearchParams();
-    const mediaItem = mediaDetailedList.find((media) => media.id === id)
+function MediaDetail() {
+  const { id } = useLocalSearchParams();
+  const mediaItem = mediaDetailedList.find(media => media.id === id);
 
-    if (!mediaItem){
-        return(
-            <Text style={{color:'white'}}>
-                Media Item is not found!
-            </Text>
-        )
-    }
-    const {title,description, releaseYear, ageRestriction, duration, type, seasons} = mediaItem
-    return(
-        <SafeAreaView>
-            <MediaInfo 
-            title={title}
-            releaseYear={releaseYear}
-            ageRestriction={ageRestriction}
-            duration={duration}
-            type={type}
-            desc={description}
-            nrOfSeasons={seasons?.length}
-            />
-        </SafeAreaView>
-    )
+  if (!mediaItem) {
+    return <Text style={{ color: 'white' }}>Media Item not found!</Text>;
+  }
+
+  const { title, description, releaseYear, ageRestriction, duration, type, seasons, trailer, videoUrl } = mediaItem;
+  const videoSource = type === 'MOVIE' ? videoUrl : seasons?.[0].episodes?.[0].videoUrl;
+
+  if (!videoSource) {
+    return <Text style={{ color: 'white' }}>Media Item not found!</Text>;
+  }
+
+  const trailerPlayer = useVideoPlayer(trailer, player => {
+    player.currentTime = 10;
+    player.play();
+  });
+
+  const mediaPlayer = useVideoPlayer(videoSource, player => {
+    player.showNowPlayingNotification = true;
+  });
+
+  return (
+    <SafeAreaView>
+      <MediaHeader />
+      <MediaInfo
+        title={title}
+        releaseYear={releaseYear}
+        ageRestriction={ageRestriction}
+        duration={duration}
+        type={type}
+        desc={description}
+        nrOfSeasons={seasons?.length}
+      />
+
+
+    </SafeAreaView>
+  );
 }
 
-export default mediaDetail
+export default MediaDetail;
---
+++ src/components/mediaDetails/MediaHeader.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function MediaHeader() {
+  return (
+    <View>
+      <Text style={{ color: 'white' }}>Hello Media</Text>
+    </View>
+  );
+}
+
+export default MediaHeader;
