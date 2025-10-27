8ad2447ec8902a4a99b7e641685c2fb4d856308a
Nabilleb <nabilgreen500@gmail.com>
2025-10-27 14:59:16 +0200

    user can select which seasons

--- src/app/mediaDetail/[id].tsx
+++ src/app/mediaDetail/[id].tsx
@@ -1,72 +1,101 @@
 import { useLocalSearchParams } from "expo-router";
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import mediaDetailedList from '@assets/data/mediaDetailedList.json';
+import mediaDetailedList from "@assets/data/mediaDetailedList.json";
 import MediaInfo from "@/components/mediaDetails/MediaInfo";
 import { useVideoPlayer, VideoView } from "expo-video";
 import MediaHeader from "@/components/mediaDetails/MediaHeader";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import SeasonSelector from "@/components/mediaDetails/SeasonSelector";
+import { Episode } from "@/types/type";
 
 function MediaDetail() {
   const { id } = useLocalSearchParams();
-  const videoViewRef = useRef<VideoView | null>(null)
-const [selectedSeason, setSelectedSeason] = useState<string>("Season 1");
-const mediaItem = mediaDetailedList.find(media => media.id === id);
+  const videoViewRef = useRef<VideoView | null>(null);
+  const [selectedSeason, setSelectedSeason] = useState<string>("Season 1");
+  const [seasonEpisodes, setSeasonsEpisodes] = useState<Episode[]>([])
+
+  const mediaItem = mediaDetailedList.find((media) => media.id === id);
+  useEffect(() =>{
+if (!mediaItem || mediaItem.type !== 'TV_SERIES') return;
+const season = mediaItem.seasons?.find((seasonItem) => seasonItem.seasonName === selectedSeason)
+setSeasonsEpisodes(season?.episodes ||[])
+  },[selectedSeason])
 
   if (!mediaItem) {
-    return <Text style={{ color: 'white' }}>Media Item not found!</Text>;
+    return <Text style={{ color: "white" }}>Media Item not found!</Text>;
   }
 
-  const { title, description, releaseYear, ageRestriction, duration, type, seasons, trailer, videoUrl, thumbnail } = mediaItem;
-  const videoSource = type === 'MOVIE' ? videoUrl : seasons?.[0].episodes?.[0].videoUrl;
+  const {
+    title,
+    description,
+    releaseYear,
+    ageRestriction,
+    duration,
+    type,
+    seasons,
+    trailer,
+    videoUrl,
+    thumbnail,
+  } = mediaItem;
+
+  const videoSource =
+    type === "MOVIE" ? videoUrl : seasons?.[0].episodes?.[0].videoUrl;
 
   if (!videoSource) {
-    return <Text style={{ color: 'white' }}>Media Item not found!</Text>;
+    return <Text style={{ color: "white" }}>Media Item not found!</Text>;
   }
 
- const trailerPlayer = useVideoPlayer(trailer, player => {
-     player.currentTime = 10;
-     player.play();
- });
+  const trailerPlayer = useVideoPlayer(trailer, (player) => {
+    player.currentTime = 10;
+    player.play();
+  });
 
- const mediaPlayer = useVideoPlayer(videoSource, player => {
-     player.showNowPlayingNotification = true;
- });
+  const mediaPlayer = useVideoPlayer(videoSource, (player) => {
+    player.showNowPlayingNotification = true;
+  });
 
- const onPlayMediaPressed = () =>{
-  trailerPlayer.pause()
-  videoViewRef.current?.enterFullscreen()
-  mediaPlayer.play()
- }
-
+  const onPlayMediaPressed = () => {
+    trailerPlayer.pause();
+    videoViewRef.current?.enterFullscreen();
+    mediaPlayer.play();
+  };
 
   return (
-    <SafeAreaView>
-      <MediaHeader 
-      thumbnail={thumbnail}
-      trailerPlayer={trailerPlayer}
-      mediaPlayer={mediaPlayer}
-      videoViewRef = {videoViewRef}
+    <SafeAreaView style={{flex:1}}>
+      <MediaHeader
+        thumbnail={thumbnail}
+        trailerPlayer={trailerPlayer}
+        mediaPlayer={mediaPlayer}
+        videoViewRef={videoViewRef}
       />
-      <MediaInfo
-        title={title}
-        releaseYear={releaseYear}
-        ageRestriction={ageRestriction}
-        duration={duration}
-        type={type}
-        desc={description}
-        nrOfSeasons={seasons?.length}
-        onPlayMediaPressed={onPlayMediaPressed}
-      />
-{type === 'TV_SERIES' && !!seasons && (
-  <SeasonSelector 
-    seasons={seasons} 
-    selectedSeason={selectedSeason}
-  />
-)}
 
-   
+      <FlatList
+        data={seasonEpisodes} 
+        renderItem={({ item }) => <Text style={{color:'white'}}>fjf</Text>}
+        ListHeaderComponent={
+          <>
+            <MediaInfo
+              title={title}
+              releaseYear={releaseYear}
+              ageRestriction={ageRestriction}
+              duration={duration}
+              type={type}
+              desc={description}
+              nrOfSeasons={seasons?.length}
+              onPlayMediaPressed={onPlayMediaPressed}
+            />
+
+            {type === "TV_SERIES" && !!seasons && (
+              <SeasonSelector
+                seasons={seasons}
+                selectedSeason={selectedSeason}
+                setSelectedSeason={setSelectedSeason}
+              />
+            )}
+          </>
+        }
+      />
     </SafeAreaView>
   );
 }
--- src/components/mediaDetails/SeasonSelector.tsx
+++ src/components/mediaDetails/SeasonSelector.tsx
@@ -1,68 +1,81 @@
-import { Entypo } from "@expo/vector-icons"
+import { Entypo } from "@expo/vector-icons";
 import { useState } from "react";
-import { View, Text, Pressable, StyleSheet } from "react-native"
-import { Menu } from "react-native-paper"
+import { View, Text, Pressable, StyleSheet } from "react-native";
+import { Menu } from "react-native-paper";
 import { Season } from "@/types/type";
 
-type SeasonSelectorProps ={
-seasons: Season[],
-selectedSeason: string,
-}
+type SeasonSelectorProps = {
+  seasons: Season[];
+  selectedSeason: string;
+  setSelectedSeason: React.Dispatch<React.SetStateAction<string>>;
+};
 
-function SeasonSelector(props){
-    const {selectedSeason, seasons} = props
-    const [isMenuVisible, setIsMenuVisible] = useState<boolean>(false);
+function SeasonSelector(props: SeasonSelectorProps) {
+  const { selectedSeason, seasons, setSelectedSeason } = props;
+  const [isMenuVisible, setIsMenuVisible] = useState<boolean>(false);
 
-return(
-    
+  const onHandleSeasonChange = (newSeason: string) => {
+    setSelectedSeason(newSeason);
+    setIsMenuVisible(false);
+  };
 
-
-<Menu
-  visible={isMenuVisible}
-  onDismiss={() => setIsMenuVisible(false)}
-  contentStyle={styles.menuContent}
-  anchorPosition="bottom"
-  anchor={
-    <Pressable
-      style={styles.anchorContainer}
-      onPress={() => setIsMenuVisible(true)}
-    >
-      <Text style={styles.SelectSeasonText}>Season 1</Text>
-      <Entypo name="chevron-thin-down" size={15} color="white" />
-    </Pressable>
-  }
->
-  <Menu.Item title="test" titleStyle={{ color: "white" }} />
-</Menu>
-
-
-)
+  return (
+    <View style={{ zIndex: 10 }}> 
+      <Menu
+        visible={isMenuVisible}
+        onDismiss={() => setIsMenuVisible(false)}
+        contentStyle={styles.menuContent}
+        anchorPosition="bottom"
+        anchor={
+          <Pressable
+            style={styles.anchorContainer}
+            onPress={() => setIsMenuVisible(true)}
+          >
+            <Text style={styles.SelectSeasonText}>{selectedSeason}</Text>
+            <Entypo name="chevron-thin-down" size={15} color="white" />
+          </Pressable>
+        }
+      >
+        {seasons?.map((season) => (
+          <Menu.Item
+            key={season.seasonName}
+            title={season.seasonName}
+            titleStyle={styles.menuStyle}
+            style={{ height: 40 }}
+            onPress={() => onHandleSeasonChange(season.seasonName)}
+          />
+        ))}
+      </Menu>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-menuContent: {
-    backgroundColor: '#282828',
+  menuContent: {
+    backgroundColor: "#282828",
     marginTop: 5,
     borderRadius: 10,
-},
+  },
+  anchorContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#282828",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 3,
+    gap: 5,
+    marginTop: 10,
+    marginLeft: 10,
+    alignSelf: "flex-start",
+  },
+  SelectSeasonText: {
+    color: "white",
+    fontWeight: "500",
+  },
+  menuStyle: {
+    color: "white",
+    fontWeight: "500",
+  },
+});
 
-anchorContainer:{
-flexDirection:'row',
-alignContent:'center',
-backgroundColor:'#282828',
-paddingHorizontal: 10,
-paddingVertical: 5,
-borderRadius: 3,
-gap: 5,
-marginTop: 10,
-marginLeft:10,
-alignSelf: 'flex-start'
-},
-
-SelectSeasonText:{
-    color: 'white',
-    fontWeight:500,
-
-}
-})
-export default SeasonSelector
+export default SeasonSelector;
